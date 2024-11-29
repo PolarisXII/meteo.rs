89657878716ab7abb9d1adf717d709d45dfb02f6
Sylvia Wang <sylviamiaowang@gmail.com>
2024-11-29 22:03:24 +1100

    deleted redundant comments

--- src/main.rs
+++ src/main.rs
@@ -33,17 +33,6 @@ impl EventHandler for Handler {
         }
     }
 
-    // async fn message(&self, ctx: Context, msg: Message) {
-    //     if msg.content == "!ping" {
-    //         // Sending a message can fail, due to a network error, an authentication error, or lack
-    //         // of permissions to post in the channel, so log to stdout when some error happens,
-    //         // with a description of it.
-    //         if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-    //             println!("Error sending message: {why:?}");
-    //         }
-    //     }
-    // }
-
     async fn ready(&self, context: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
