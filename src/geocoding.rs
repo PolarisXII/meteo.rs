39dd37895a1c95985128f0ff0044571e72bf4424
Sylvia Wang <sylviamiaowang@gmail.com>
2024-11-29 22:02:55 +1100

    cleaned up error handling

--- src/geocoding.rs
+++ src/geocoding.rs
@@ -10,20 +10,20 @@ struct Geolocation {
 }
 pub async fn get_geolocation_from_city(city: String) -> Result<(f64, f64), Box<dyn Error>> {
     dotenv().ok();
-    let api_key =
-        std::env::var("OPEN_WEATHER_API_KEY").expect("Expected an API key in the environment");
+    let api_key = std::env::var("OPEN_WEATHER_API_KEY")?;
     let url = format!(
         "http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}",
         city = city,
         api_key = api_key
     );
 
-    let response = reqwest::get(&url).await.expect("Failed to send request");
+    let response = reqwest::get(&url).await?;
     if let Err(why) = response.error_for_status_ref() {
         Err(why)?;
     }
 
-    let geolocation: Vec<Geolocation> = response.json().await.expect("Failed to parse response");
+    let geolocation: Vec<Geolocation> = response.json().await?;
+
     println!("{:?}", geolocation);
     return Ok((geolocation[0].lat, geolocation[0].lon));
 }
--- src/weather.rs
+++ src/weather.rs
@@ -6,53 +6,52 @@ use std::error::Error;
 
 #[derive(Deserialize, Debug)]
 pub struct Weather {
-    temp: f64,
-    feels_like: f64,
-    temp_min: f64,
-    temp_max: f64,
-    humidity: f64,
+    pub temp: f64,
+    pub feels_like: f64,
+    pub temp_min: f64,
+    pub temp_max: f64,
+    pub humidity: f64,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct WeatherDescription {
-    main: String,
-    description: String,
+    pub main: String,
+    pub description: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Rain {
-    one_hour: Option<f64>,
+    pub one_hour: Option<f64>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Wind {
-    speed: f64,
+    pub speed: f64,
 }
 #[derive(Deserialize, Debug)]
 pub struct WeatherResponse {
-    weather: Vec<WeatherDescription>,
-    main: Weather,
-    wind: Wind,
-    rain: Option<Rain>,
+    pub weather: Vec<WeatherDescription>,
+    pub main: Weather,
+    pub wind: Wind,
+    pub rain: Option<Rain>,
 }
 pub async fn get_current_weather_for_location(
     lat: f64,
     long: f64,
 ) -> Result<WeatherResponse, Box<dyn Error>> {
     dotenv().ok();
-    let api_key =
-        std::env::var("OPEN_WEATHER_API_KEY").expect("Expected an API key in the environment");
+    let api_key = std::env::var("OPEN_WEATHER_API_KEY")?;
     let url = format!(
         "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}&units=metric",
         lat = lat.to_string(),
         long = long.to_string(),
         api_key = api_key
     );
-    let response = reqwest::get(&url).await.expect("Failed to send request");
+    let response = reqwest::get(&url).await?;
     if let Err(why) = response.error_for_status_ref() {
         Err(why)?
     } else {
-        let weather: WeatherResponse = response.json().await.expect("Failed to parse response");
+        let weather: WeatherResponse = response.json().await?;
 
         return Ok(weather);
     }
